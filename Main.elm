c3a3d78f6e18c00ffba1256bca27b33025d14fc9
josephscottstevens <josephscottstevens@gmail.com>
2018-05-08 11:51:35 -0400

    t

--- Main.elm
+++ Main.elm
@@ -3,15 +3,10 @@ port module Main exposing (main)
 import Common.Table as Table exposing (ColumnStyle(CustomStyle, Width))
 import Html exposing (Html, div, h1, input, text)
 import Html.Attributes exposing (type_)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, onClick)
 
 
-port loadData : (List Row -> msg) -> Sub msg
-
-
-cust_codes : List String
-cust_codes =
-    [ "ABJ010", "DJA414", "GXD831", "ABJ002", "RANDO" ]
+port loadData : (( List Row, List CustomerData ) -> msg) -> Sub msg
 
 
 subscriptions : Model -> Sub Msg
@@ -19,30 +14,39 @@ subscriptions model =
     loadData LoadData
 
 
+type alias CustomerData =
+    { code : String
+    , client_active : Bool
+    }
+
+
 type alias Row =
-    { content_id : Int
-    , cust_codes : List String
+    { contentId : Int
+    , customerCodes : List String
     }
 
 
 type alias Model =
     { rows : List Row
+    , clients : List CustomerData
     , tableState : Table.State
     , filterStr : String
+    , showInactive : Bool
     }
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ Table.view model.tableState model.rows (gridConfig model.filterStr)
+        [ Table.view model.tableState model.rows (gridConfig model)
         ]
 
 
 type Msg
     = SetTableState Table.State
-    | LoadData (List Row)
+    | LoadData ( List Row, List CustomerData )
     | UpdateFilter String
+    | ToggleShowInactive
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -53,8 +57,8 @@ update msg model =
             , Cmd.none
             )
 
-        LoadData rows ->
-            ( { model | rows = rows }
+        LoadData ( rows, clients ) ->
+            ( { model | rows = rows, clients = clients }
             , Cmd.none
             )
 
@@ -63,37 +67,55 @@ update msg model =
             , Cmd.none
             )
 
+        ToggleShowInactive ->
+            ( { model | showInactive = not model.showInactive }
+            , Cmd.none
+            )
+
 
 rowHelper : String -> Row -> Maybe String
 rowHelper custCode row =
-    if List.member custCode row.cust_codes then
+    if List.member custCode row.customerCodes then
         Just "X"
     else
         Nothing
 
 
-gridConfig : String -> Table.Config Row Msg
-gridConfig filterStr =
-    { domTableId = "PresidentsTable"
-    , toolbar = input [ type_ "text", onInput UpdateFilter ] []
-    , toMsg = SetTableState
-    , columns =
-        [ Table.stringColumn "Content ID" (\t -> Just (toString t.content_id)) (Width 1)
-        ]
-            ++ List.map
-                (\custCode ->
-                    Table.stringColumn custCode (\t -> rowHelper custCode t) (Width 1)
-                )
-                (List.filter (\t -> String.contains filterStr t) cust_codes)
-    , toRowId = .content_id
-    }
+gridConfig : Model -> Table.Config Row Msg
+gridConfig model =
+    let
+        filteredColumns =
+            if model.showInactive == True then
+                List.filter (\t -> String.contains model.filterStr t.code) model.clients
+            else
+                List.filter (\t -> String.contains model.filterStr t.code && t.client_active == True) model.clients
+    in
+        { domTableId = "PresidentsTable"
+        , toolbar =
+            div []
+                [ input [ type_ "text", onInput UpdateFilter ] []
+                , input [ type_ "checkbox", onClick ToggleShowInactive ] []
+                ]
+        , toMsg = SetTableState
+        , columns =
+            [ Table.stringColumn "Content ID" (\t -> Just (toString t.contentId)) (Width 1)
+            ]
+                ++ List.map
+                    (\customer ->
+                        Table.stringColumn customer.code (\t -> rowHelper customer.code t) (Width 1)
+                    )
+                    filteredColumns
+        , toRowId = .contentId
+        }
 
 
 init : ( Model, Cmd msg )
 init =
-    ( { rows = [ Row 1 [ "A", "ABJ002", "C" ] ]
+    ( { rows = []
+      , clients = []
       , tableState = Table.init "Year"
       , filterStr = ""
+      , showInactive = True
       }
     , Cmd.none
     )
